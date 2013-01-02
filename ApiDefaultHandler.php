9930a31691d258a398db6438e20ee4aa5075951d
tomhalley89 <tomhalley89@gmail.com>
2013-01-02 15:47:23 +0000

    cleaned up some horrendously bad commenting

--- ApiDefaultHandler.php
+++ ApiDefaultHandler.php
@@ -13,22 +13,21 @@ namespace LWare\Queue;
  * @copyright Copyright (c) 2012 CGentry
  * @version   1.0
  * @since     2012-Dec-18
- */
-/*
-                This file is part of LWare\Queue.
-
-    LWare\Queue is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Lesser Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    LWare\Queue is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU Lesser Public License
-    along with LWare\Queue.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * This file is part of LWare\Queue.
+ *
+ * LWare\Queue is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LWare\Queue is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with LWare\Queue.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 
